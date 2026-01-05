1520e04051d00aca049784e62b2e53b36940dea3
7minutech <elijohnson1107@gmail.com>
2026-01-04 22:23:22 -0600

    refac: getObjectURL returns bucket,key not https url

--- assets.go
+++ assets.go
@@ -36,5 +36,5 @@ func mediaTypeToExt(mediaType string) string {
 }
 
 func (cfg apiConfig) getObjectURL(key string) string {
-	return fmt.Sprintf("https://%s.s3.%s.amazonaws.com/%s", cfg.s3Bucket, cfg.s3Region, key)
+	return fmt.Sprintf("%s,%s", cfg.s3Bucket, key)
 }
