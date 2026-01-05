61fc2d97e7772bec32f3b31e8296c0571137df34
7minutech <elijohnson1107@gmail.com>
2026-01-04 22:51:53 -0600

    feat: dbVideoToSignedVideo

---
+++ dbVideoToSignedVideo.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/database"
+)
+
+func (cfg *apiConfig) dbVideoToSignedVideo(video database.Video) (database.Video, error) {
+	if video.VideoURL == nil {
+		return video, fmt.Errorf("video url must be not nil")
+	}
+
+	videoParts := strings.Split(*video.VideoURL, ",")
+	if len(videoParts) != 2 {
+		return video, fmt.Errorf("video url must have bucket,key")
+	}
+
+	bucket := videoParts[0]
+	key := videoParts[1]
+
+	presignedURL, err := generatePresignedURL(cfg.s3Client, bucket, key, time.Minute*10)
+	if err != nil {
+		return video, err
+	}
+
+	video.VideoURL = aws.String(presignedURL)
+
+	return video, nil
+}
